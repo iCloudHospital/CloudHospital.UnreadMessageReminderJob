ca4f9a184dd757f2c7b3897f27bded0a096e4d48
Pon Cheol Ku (구본철) <bbon72@icloudhospital.com>
2022-09-13 12:10:22 +0900

    Change UnreadNotification to target CHManager #26
    - Change gathering receiver information

--- Functions/SendUnreadMessageReminder.cs
+++ Functions/SendUnreadMessageReminder.cs
@@ -65,88 +65,94 @@ public class SendUnreadMessageReminder : FunctionBase
             throw new Exception(logMessage);
         }
 
-        // sender == [ChManager, Manager]
-        var userType = item.Sender?.Metadata?.UserType;
+        var senderUserType = item.Sender?.Metadata?.UserType;
+        var senderUserId = item.Sender?.UserId;
+        var members = item.Members
+            .Where(x => x.UserId != senderUserId);
 
-        var member = item.Members
-            .FirstOrDefault(x => string.IsNullOrWhiteSpace(x.Metadata?.UserType));
-
-        if (IsInDebug && member == null)
+        if (IsInDebug && !members.Any())
         {
-            _logger.LogInformation("Member does not found.");
+            _logger.LogInformation("⚠️ Members who will receive notification does not found.");
         }
 
-        // find user email
-        var userId = member?.UserId;
-        var hospitalId = item.Channel.CustomType;
-
-        if (IsInDebug)
+        foreach (var member in members)
         {
-            _logger.LogInformation("🔨 userId={userId}, hospitalId={hospitalId}", userId, hospitalId);
-        }
+            // find user email
+            var userId = member?.UserId;
+            var hospitalId = item.Channel.CustomType;
 
-        var user = await GetUser(userId);
-        var hospital = await GetHospitalAsync(hospitalId);
+            var userTypeForTemplate = member?.Metadata?.UserType;
 
-        if (user == null)
-        {
-            if (IsInDebug)
+            if (string.IsNullOrWhiteSpace(userTypeForTemplate))
             {
-                _logger.LogWarning("User does not find.");
+                userTypeForTemplate = senderUserType;
             }
-        }
-        else if (hospital == null)
-        {
+
             if (IsInDebug)
             {
-                _logger.LogWarning("Hospital does not find.");
+                _logger.LogInformation("🔨 userId={userId}, hospitalId={hospitalId}, userTypeForTemplate={userType}", userId, hospitalId, userTypeForTemplate);
             }
-        }
-        else
-        {
-            if (IsInDebug)
+
+            var user = await GetUser(userId);
+            var hospital = await GetHospitalAsync(hospitalId);
+
+            if (user == null)
             {
-                _logger.LogInformation(@$"🔨 User:
+                if (IsInDebug)
+                {
+                    _logger.LogWarning("User does not find. (userId={userId})", userId);
+                }
+            }
+            else if (hospital == null)
+            {
+                if (IsInDebug)
+                {
+                    _logger.LogWarning("Hospital does not find. (hospitalId={hospitalID})", hospitalId);
+                }
+            }
+            else
+            {
+                if (IsInDebug)
+                {
+                    _logger.LogInformation(@$"🔨 User:
 {nameof(UserModel.Id)}={user.Id}
 {nameof(UserModel.Email)}={user.Email}
 {nameof(UserModel.FullName)}={user.FullName}
 ");
-                _logger.LogInformation(@"🔨 Hospital: 
+                    _logger.LogInformation(@"🔨 Hospital: 
 Name: {name}
 Logo: {logo}
 website: {website}
 ", hospital.Name, hospital.Logo, hospital.WebsiteUrl);
-            }
-
-            // send email using sendgrid template
-            var message = item.Payload?.Message ?? "You have unread chat message. Please check your message our application.";
-
-            var cloudHospitalBaseUrl = Environment.GetEnvironmentVariable(Constants.ENV_CLOUDHOSPITAL_BASEURL);
-            var targetPage = $"{cloudHospitalBaseUrl}/?chat=true";
-
-            var templateData = GetTemplateData(user, item, hospital, message, targetPage);
-
-            if (templateData == null)
-            {
-                _logger.LogInformation($"Not supported userType [userType={userType}]");
-            }
-            else
-            {
-
-                if (IsInDebug)
-                {
-                    _logger.LogInformation(@$"🔨 Email Template data: 
-{JsonSerializer.Serialize(templateData, _jsonSerializerOptions)}
-");
                 }
 
-                await _emailSender.SendEmailAsync(user.Email, user.FullName, emailTemplateId, templateData);
+                // send email using sendgrid template
+                var message = item.Payload?.Message ?? "You have unread chat message. Please check your message our application.";
 
-                _logger.LogInformation("✅ Unread message reminder job completed.");
+                var cloudHospitalBaseUrl = Environment.GetEnvironmentVariable(Constants.ENV_CLOUDHOSPITAL_BASEURL);
+                var targetPage = $"{cloudHospitalBaseUrl}/?chat=true";
+
+                var templateData = GetTemplateData(userTypeForTemplate, user, item, hospital, message, targetPage);
+
+                if (templateData == null)
+                {
+                    _logger.LogInformation($"Not supported userType [userType={senderUserType}]");
+                }
+                else
+                {
+                    if (IsInDebug)
+                    {
+                        _logger.LogInformation(@"🔨 Email Template data: 
+{json}
+", JsonSerializer.Serialize(templateData, _jsonSerializerOptions));
+                    }
+
+                    await _emailSender.SendEmailAsync(user.Email, user.FullName, emailTemplateId, templateData);
+
+                    _logger.LogInformation("✅ Unread message reminder job completed.");
+                }
             }
-
         }
-
     }
 
     private async Task<UserModel?> GetUser(string? id)
@@ -157,8 +163,6 @@ website: {website}
 
             return null;
         }
-
-
 
         using (var connection = new SqlConnection(_databaseConfiguration.ConnectionString))
         {
@@ -235,46 +239,7 @@ AND h.Id = @HospitalId
         }
     }
 
-    [Obsolete("Use Channel.CustomType")]
-    private string? GetHospitalIdFromChannelUrl(string channelUrl)
-    {
-        // channel_url 
-        // CH: {hospitalId}--{userId}
-        // 9b722461-dfa0-42ba-d069-08d7f631e33c--5d945b83-83fd-434c-baba-9aac88014c85
-        // 
-        // Etc: external--{hospitalId}--{userId}
-        // external--bae1b5bc-325d-45fb-6b91-08d93a6658e7--970aaa4d-0997-4b4d-bc04-df07f6a17906
-
-        var tokens = channelUrl.Split("--", StringSplitOptions.RemoveEmptyEntries);
-
-        if (tokens.Length > 2)
-        {
-            return tokens[1];
-        }
-        else if (tokens.Length > 1)
-        {
-            return tokens[0];
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    private object GetSampleTemplateData(string toName, string fromName, string message, DateTime? created)
-    {
-        var templateData = new
-        {
-            To = toName,
-            From = fromName,
-            Message = message,
-            Created = created
-        };
-
-        return templateData;
-    }
-
-    private object? GetTemplateData(UserModel user, SendBirdGroupChannelMessageSendEventModel item, HospitalModel hospital, string? message = null, string? targetPage = null) => item?.Sender?.Metadata?.UserType switch
+    private object? GetTemplateData(string userTypeForTemplate, UserModel user, SendBirdGroupChannelMessageSendEventModel item, HospitalModel hospital, string? message = null, string? targetPage = null) => userTypeForTemplate switch
     {
         SendBirdSenderUserTypes.ChManager => GetChManagerTemplateData(user, item, hospital, message, targetPage),
         SendBirdSenderUserTypes.Manager => GetManagerTemplateData(user, item, hospital, message, targetPage),
@@ -363,5 +328,6 @@ public class UnreadMessageReminderEmialTemplateData
     public string Message { get; set; } = string.Empty;
 
     public DateTime? Created { get; set; } = DateTime.UtcNow;
+
     public string TargetPage { get; set; } = string.Empty;
 }
